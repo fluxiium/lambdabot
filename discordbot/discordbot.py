2d69a77f5d32c96f5453d98ff4beba4ecfbb21b2
yackson <2530617+realyackson@users.noreply.github.com>
2017-08-17 22:22:16 +0200

    add website to help

--- discordbot/discordbot.py
+++ discordbot/discordbot.py
@@ -37,7 +37,8 @@ async def on_message(message):
     if message.content == '!help' or client.user in message.mentions:
         await client.send_message(message.channel, 'available commands:\n'
                                                    '`!meme` - generate a random meme\n'
-                                                   '`!help` - show this text')
+                                                   '`!help` - show this text\n'
+                                                   'https://lambdabot.morchkovalski.com')
 
     elif message.content.startswith('!meme'):
         await client.send_typing(message.channel)
