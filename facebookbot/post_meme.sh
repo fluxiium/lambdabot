ceabc104bb92185130fa28bb55dc209e10c9c02f
yackson <2530617+realyackson@users.noreply.github.com>
2017-08-17 19:32:37 +0200

    ffs

--- facebookbot/post_meme.sh
+++ facebookbot/post_meme.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /srv/lambdabot.morchkovalski.com/venv/bin/activate
-PYTHONPATH=/srv/lambdabot.morchkovalski.com/lambdabot python ./srv/lambdabot.morchkovalski.com/lambdabot/facebookbot/post_meme.py
+PYTHONPATH=/srv/lambdabot.morchkovalski.com/lambdabot python /srv/lambdabot.morchkovalski.com/lambdabot/facebookbot/post_meme.py
