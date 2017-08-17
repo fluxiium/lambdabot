c68da04097d93ffdad941bb4448cb9c4fc5b8989
yackson <2530617+realyackson@users.noreply.github.com>
2017-08-17 21:56:52 +0200

    discord bot launcher

---
+++ discordbot/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /srv/lambdabot.morchkovalski.com/venv/bin/activate
+PYTHONPATH=/srv/lambdabot.morchkovalski.com/lambdabot python /srv/lambdabot.morchkovalski.com/lambdabot/discordbot/discordbot.py
