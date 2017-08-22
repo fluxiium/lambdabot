a891a81f6b7e0b7638c95948132f454a969be9f4
yackson <2530617+realyackson@users.noreply.github.com>
2017-08-22 03:12:11 +0200

    twitterbot

---
+++ twitterbot/post_meme.py
@@ -0,0 +1,24 @@
+import twitter
+
+from lambdabot.preview import preview_meme
+from lambdabot.db import make_meme
+from lambdabot.settings import *
+
+token_file = open(os.path.join(DATA_DIR, 'twittertokens.txt'), 'r')
+TOKENS = token_file.read().splitlines()
+token_file.close()
+
+api = twitter.Api(consumer_key=TOKENS[0],
+                  consumer_secret=TOKENS[1],
+                  access_token_key=TOKENS[2],
+                  access_token_secret=TOKENS[3])
+
+meme_id = make_meme(context='facebook')
+preview_meme(meme_id)
+
+status = api.PostUpdate(
+    "https://lambdabot.morchkovalski.com/meme_info/" + meme_id,
+    media=open(os.path.join(DATA_DIR, 'previews', meme_id + '.jpg'), 'rb')
+)
+print("post added!")
+print(status.id)
