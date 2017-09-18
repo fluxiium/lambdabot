64e00276044c1cbf163f924df31721d75e496788
yackson <2530617+realyackson@users.noreply.github.com>
2017-09-18 18:06:31 +0200

    discordbot dtach

--- discordbot/start.sh
+++ discordbot/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+stty rows 30
+stty cols 100
 source /srv/lambdabot.morchkovalski.com/venv/bin/activate
 PYTHONPATH=/srv/lambdabot.morchkovalski.com/lambdabot python /srv/lambdabot.morchkovalski.com/lambdabot/discordbot/start.py
