32f630e0338d5cbcbc6cd23294dbb94eaf1ef1fe
yackson <2530617+realyackson@users.noreply.github.com>
2017-09-22 21:56:40 +0200

    discordbot start loop

--- discordbot/start.sh
+++ discordbot/start.sh
@@ -3,4 +3,7 @@ stty rows 30
 stty cols 100
 source /srv/lambdabot.morchkovalski.com/venv/bin/activate
 cd /srv/lambdabot.morchkovalski.com/lambdabot/discordbot
-PYTHONPATH=/srv/lambdabot.morchkovalski.com/lambdabot python start.py
+while true; do
+    PYTHONPATH=/srv/lambdabot.morchkovalski.com/lambdabot python start.py
+    sleep 5
+done
