583aef3474504d1485a848fc010aa24a419fd89d
yackson <2530617+realyackson@users.noreply.github.com>
2018-03-30 23:48:26 +0200

    garfield cog

---
+++ discordbot/cogs/garfield.py
@@ -0,0 +1,58 @@
+import json
+import requests
+
+from discord.ext import commands
+from discord.ext.commands import Bot, Context, CommandError
+from discordbot.util import headers
+
+
+class GarfieldCog:
+    def __init__(self, bot: Bot):
+        self.cog_name = "Garfield"
+        self.bot = bot
+
+    @commands.command(name='wiki', help='search the garfield wiki')
+    async def _cmd_wiki(self, ctx: Context, *, query=None):
+        wiki_url = 'http://garfield.wikia.com'
+        article_url = None
+
+        if not query:
+            response = requests.get(
+                '{0}/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json'.format(wiki_url),
+                headers=headers,
+            )
+            article_data = json.loads(response.content.decode('utf-8'))
+            article_id = article_data['query']['random'][0]['id']
+
+            response = requests.get(
+                '{0}/api/v1/Articles/Details?ids={1}'.format(wiki_url, article_id),
+                headers=headers,
+            )
+            article_data = json.loads(response.content.decode('utf-8'))
+            article_url = "{0}{1}".format(
+                wiki_url,
+                article_data['items'][str(article_id)]['url']
+            )
+
+        else:
+            response = requests.get(
+                '{0}/api/v1/Search/List?query={1}&limit=1'.format(wiki_url, query),
+                headers=headers,
+            )
+            article_data = json.loads(response.content.decode('utf-8'))
+            if article_data.get('exception') is None:
+                article_url = article_data['items'][0]['url']
+
+        if article_url is None:
+            raise CommandError("article not found :cry:")
+        else:
+            await ctx.send("{} {}".format(ctx.author.mention, article_url))
+
+    @_cmd_wiki.error
+    async def _wiki_error(self, ctx: Context, error):
+        if isinstance(error, CommandError):
+            await ctx.send("{} {}".format(ctx.author.mention, str(error)))
+
+
+def setup(bot: Bot):
+    bot.add_cog(GarfieldCog(bot))
