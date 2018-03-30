3cfbb54f87af375d5eee7035bc567b2db0071f8e
yackson <2530617+realyackson@users.noreply.github.com>
2018-03-30 18:35:16 +0200

    customcmd fix

--- discordbot/cogs/customcmd.py
+++ discordbot/cogs/customcmd.py
@@ -1,4 +1,5 @@
-from discord.ext.commands import Bot, Context
+from discord import Message
+from discord.ext.commands import Bot
 from django.core.exceptions import ObjectDoesNotExist
 from discordbot.models import DiscordServer
 
@@ -8,21 +9,21 @@ class CustomCmds:
         self.bot = bot
 
     # noinspection PyMethodMayBeStatic
-    async def on_command_error(self, ctx: Context, exc):
+    async def on_message(self, msg: Message):
         try:
-            server_data = DiscordServer.get(ctx.guild)
+            server_data = DiscordServer.get(msg.guild)
         except ObjectDoesNotExist:
             return
 
-        msg_text = ctx.message.content.strip()
+        msg_text = msg.content.strip()
 
-        if not msg_text.startswith(ctx.prefix) or ctx.author.bot:
+        if not msg_text.startswith(self.bot.command_prefix) or msg.author.bot:
             return
 
-        cmd = server_data.get_cmd(ctx.invoked_with)
+        cmd = server_data.get_cmd(msg_text[len(self.bot.command_prefix):])
         if cmd is None:
             return
-        await ctx.send(cmd.message)
+        await msg.channel.send(cmd.message)
 
 
 def setup(bot: Bot):
