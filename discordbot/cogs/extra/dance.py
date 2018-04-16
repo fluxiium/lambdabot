d9388971dc111b35f2877e26ac300cdb4333d9cb
yackson <2530617+realyackson@users.noreply.github.com>
2018-04-16 02:10:49 +0200

    oops

--- discordbot/cogs/extra/dance.py
+++ discordbot/cogs/extra/dance.py
@@ -39,5 +39,6 @@ def dance(text):
             frame = frame.resize([int(scale_factor * d) for d in frame.size], Image.ANTIALIAS)
         frame.save('%s/%d.png' % (tmpdir, i))
     # convert -dispose previous -delay 20 -loop 0 *.png img.gif
-    subprocess.check_call('"{0}" -dispose 2 -delay 20 -loop 0 "{1}/*.png" "{1}/dance.gif"'.format(config.IMAGEMAGICK_PATH, tmpdir))
+    process = subprocess.Popen('"{0}" -dispose 2 -delay 20 -loop 0 "{1}/*.png" "{1}/dance.gif"'.format(config.IMAGEMAGICK_PATH, tmpdir), shell=True, stdout=subprocess.PIPE)
+    process.wait()
     return tmpdir
