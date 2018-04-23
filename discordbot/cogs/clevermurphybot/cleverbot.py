988403e288e7b295ba8a42e3ab61f22ecc6b390c
yackson <2530617+realyackson@users.noreply.github.com>
2018-04-24 00:13:00 +0200

    prevent infinite loop

--- discordbot/cogs/clevermurphybot/cleverbot.py
+++ discordbot/cogs/clevermurphybot/cleverbot.py
@@ -1,7 +1,8 @@
 import discord
 import asyncio
 import config
-from util import log
+from discord.ext.commands import CommandError
+from util import log, log_exc
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, WebDriverException
 from selenium.webdriver.common.keys import Keys
@@ -14,25 +15,25 @@ def is_active():
     return config.CLEVERBOT_ENABLED
 
 
-def _get_driver(user: discord.User):
-    if _cb_conversations.get(user.id) is None:
-        log("creating session for {}".format(user), tag="cleverbot")
-        try:
+def _get_driver(user: discord.User, attempt=5):
+    try:
+        driver = _cb_conversations.get(user.id)
+        if not driver:
+            log("creating session for {}".format(user), tag="cleverbot")
             driver = webdriver.Firefox()
             driver.get('http://www.cleverbot.com/')
             _cb_conversations[user.id] = driver
             return driver
-        except WebDriverException:
-            asyncio.sleep(1)
-            return _get_driver(user)
-    else:
-        driver = _cb_conversations[user.id]
-        try:
+        else:
             assert 'Cleverbot.com' in driver.title
             return driver
-        except WebDriverException:
-            _cb_conversations.pop(user.id)
-            return _get_driver(user)
+    except WebDriverException as exc:
+        _cb_conversations[user.id] = None
+        if attempt > 0:
+            return _get_driver(user, attempt - 1)
+        else:
+            log_exc(exc)
+            raise exc
 
 
 async def talk(msg: discord.Message, msg_text):
@@ -41,22 +42,26 @@ async def talk(msg: discord.Message, msg_text):
 
     user = msg.author
     channel = msg.channel
+    response = 'error :cry:'
 
     async with msg.channel.typing():
-        driver = _get_driver(user)
-        input_box = driver.find_element_by_name('stimulus')
-        input_box.clear()
-        input_box.send_keys(msg_text)
-        input_box.send_keys(Keys.RETURN)
-        while True:
-            asyncio.sleep(1)
-            try:
-                driver.find_element_by_id('snipTextIcon')
-                break
-            except NoSuchElementException:
-                pass
-        response_elem = driver.find_element_by_xpath('//*[@id="line1"]/span[1]')
-        response = response_elem.text
+        try:
+            driver = _get_driver(user)
+            input_box = driver.find_element_by_name('stimulus')
+            input_box.clear()
+            input_box.send_keys(msg_text)
+            input_box.send_keys(Keys.RETURN)
+            while True:
+                asyncio.sleep(1)
+                try:
+                    driver.find_element_by_id('snipTextIcon')
+                    break
+                except NoSuchElementException:
+                    pass
+            response_elem = driver.find_element_by_xpath('//*[@id="line1"]/span[1]')
+            response = response_elem.text
+        except WebDriverException:
+            pass
 
     if msg.guild is None:
         await channel.send(response)
