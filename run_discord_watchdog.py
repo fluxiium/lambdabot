05b251a8ae4b5a001a9a8ca1539751fb960945dc
yackson <2530617+realyackson@users.noreply.github.com>
2018-06-08 16:44:55 +0200

    oops

--- run_discord_watchdog.py
+++ run_discord_watchdog.py
@@ -16,9 +16,11 @@ logging.info(f'bot process started: {bot_process.pid}')
 class WatchdogClient(discord.Client):
 
     async def on_member_update(self, _, member: discord.Member):
+        global bot_process
         if str(member.id) == config.DISCORD_BOT_ID and member.status == discord.Status.offline:
             bot_process.kill()
-            logging.info(f'bot process killed')
+            bot_process = subprocess.Popen([sys.executable, os.path.join(config.BASE_DIR, "run_discord_bot.py")])
+            logging.info(f'bot process restarted')
 
 bot = WatchdogClient()
 bot.run(config.DISCORD_WATCHDOG_TOKEN)
