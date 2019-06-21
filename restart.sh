50164b99746f69d087b0dd54f0f3815bf523044a
yackson <2530617+realyackson@users.noreply.github.com>
2019-06-21 02:52:24 +0200

    update&restart

--- restart.sh
+++ restart.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -e
+venv/bin/python manage.py collectstatic --noinput
+venv/bin/python manage.py compress
+venv/bin/python manage.py migrate
 sudo service apache2 reload
 sudo systemctl restart lambdabot.service
--- update.sh
+++ update.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 set -e
 venv/bin/pip install -r requirements.txt --upgrade
-venv/bin/python manage.py collectstatic --noinput
-venv/bin/python manage.py compress
-venv/bin/python manage.py migrate
 ./restart.sh
