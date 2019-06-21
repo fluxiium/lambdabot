390015a35ef0a0d49b08d04a83c645c0e8d9133a
yackson <2530617+realyackson@users.noreply.github.com>
2019-06-21 13:50:59 +0200

    disable django admin login page

--- lamdabotweb/urls.py
+++ lamdabotweb/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import RedirectView
 from lamdabotweb import settings
 
 urlpatterns = [
+    path('admin/login/', RedirectView.as_view(url=settings.LOGIN_URL, permanent=True, query_string=True)),
     path('admin/', admin.site.urls),
     path('discord_oauth2/', include('discord_oauth2.urls')),
     path('', include('website.urls')),
